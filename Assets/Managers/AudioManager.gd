ffcd79d8b6a3be5364ddb1ff31857ab6d14059c7
Krystof Klestil <krystofklestil@live.com>
2021-11-23 20:13:07 +0100

    ADD: Utility functions to the audio manager

--- Assets/Managers/AudioManager.gd
+++ Assets/Managers/AudioManager.gd
@@ -1,3 +1,25 @@
 extends Node
 
 # Global audio manager
+
+const VO_PATH : = "res://Assets/VO/"
+const VO_EXT : = ".wav"
+
+
+enum SFX {}
+
+
+func play_bgm(which : int) -> void:
+	$BGM.get_child(which).play()
+
+
+# Use the values from SFX enum
+func play_sfx(which : int) -> void:
+	$SFX.get_child(which).play()
+
+
+func play_voiceover(tag : String, really : bool) -> void:
+	var audio_name : String = tag.lstrip("audio_")
+	print(VO_PATH + audio_name + VO_EXT)
+	$Voiceovers/VoiceoverPlayer.stream = load(VO_PATH + audio_name + VO_EXT)
+	$Voiceovers/VoiceoverPlayer.play()
